a9efee08a9ee8e9c436def16ccbc9da4b0a49dd4
shade477 <sagardeyof477@gmail.com>
2024-10-06 20:55:18 +0530

    Created player sprite

--- player.py
+++ player.py
@@ -15,4 +15,6 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def draw(self, screen):
+        pygame.draw.polygon(screen, 'white', self.triangle(), 2)
     
