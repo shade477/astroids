9501304a16bcfd2e2a56d8a0f5d9e4f3274648d0
shade477 <sagardeyof477@gmail.com>
2024-10-06 21:09:31 +0530

    Implemented player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -8,11 +9,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y) 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
--- player.py
+++ player.py
@@ -1,14 +1,15 @@
 import circleshape
+from constants import *
 from circleshape import *
 
 class Player(CircleShape):
     def __init__(self, x, y) -> None:
-        super().__init__(PLAYER_RADIUS)
-        rotation = 0
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
 
     # in the player class
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
