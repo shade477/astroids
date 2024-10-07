9b9b64d85846a42e5bbbf8c0f25aaf7c9bea45ed
shade477 <sagardeyof477@gmail.com>
2024-10-08 00:19:45 +0530

    Fixed main.py

--- main.py
+++ main.py
@@ -14,19 +14,26 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable,drawable)
-    player = Player(x, y) 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill('black')
+        
         # player.draw(screen)
         # player.update(dt)
+
         for update_items in updatable:
             update_items.update(dt)
+
+        screen.fill('black')
+
         for draw_items in drawable:
             draw_items.draw(screen)
+
         pygame.display.flip()
+        
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
