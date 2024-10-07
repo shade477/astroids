72ed4810ed14ece9fbb5d5542b2c804494fd228d
shade477 <sagardeyof477@gmail.com>
2024-10-08 01:54:38 +0530

    Added split to asteroids

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
 
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill('black')
